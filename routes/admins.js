03a761f94313a72bbe35f36bfcba4478592773da
Bhaskar Reddy <bhaskarreddy417@gmail.com>
2019-08-26 17:05:43 +0530

    Hobbs in San Franscisco

---
+++ routes/admins.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router  = express.Router();
+var User = require("../models/user");
+var middleware = require("../middleware");
+
+
+//INDEX - show all campgrounds
+router.get("/admins/index", function(req, res){
+    // Get all campgrounds from DB
+    User.find({type: "admin"}, function(err, allAdmins){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("admins/index",{admins: allAdmins});
+       }
+    });
+});
+
+module.exports = router;
